5704eaace5ddfe7b7008fdd812b5a8f25f41f1b3
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-17 20:09:08 +0530

    added test and refactored code

--- src/test/java/org/example/StateAnalyzerTest.java
+++ src/test/java/org/example/StateAnalyzerTest.java
@@ -7,41 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class StateAnalyzerTest {
     // Test case 1.1
     @Test
-    public void testDataCountHappy() throws StateCensusException {
-        StateCensusAnalyser analyzer = new StateCensusAnalyser("StateCensus.csv");
-        int count = analyzer.countOfRecords();
+    public void HappyTestCount() {
+        StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus.csv");
+        int count = stateCensusAnalyzer.countOfRecords();
         assertEquals(count, 37);
     }
 
     // Test 1.2
     @Test
-    public void testDataCountSad() {
-        StateCensusAnalyser analyzer = new StateCensusAnalyser("StateCensus.csv");
+    public void SadTest() {
+        StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus.csv");
         int count = 3838;
         assertEquals(37, count);
-
     }
 
     // Test 1.3
     @Test
     public void testFileType() {
-        StateCensusAnalyser analyzer = new StateCensusAnalyser("StateCensus.txt");
-
-        assertEquals(37, analyzer.countOfRecords());
+        StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus.txt");
+        assertEquals(37, stateCensusAnalyzer.countOfRecords());
     }
 
     // Test 1.4
     @Test
     public void testDelimiter() {
-        StateCensusAnalyser analyzer= new StateCensusAnalyser("StateCensus2.csv");
-        analyzer.countOfRecords();
+        StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus2.csv");
+        stateCensusAnalyzer.countOfRecords();
     }
 
     // Test 1.5
     @Test
     public void testHeader() {
-        StateCensusAnalyser analyzer = new StateCensusAnalyser("StateCensus3.csv");
-        analyzer.countOfRecords();
+        StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus3.csv");
+        stateCensusAnalyzer.countOfRecords();
     }
-
 }
