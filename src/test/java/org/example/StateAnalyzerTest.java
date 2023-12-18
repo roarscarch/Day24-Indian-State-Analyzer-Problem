46bfd649c2adaf96861d4d8abbb46a21067501d7
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-18 09:58:57 +0530

    Add test cases

--- src/test/java/org/example/StateAnalyzerTest.java
+++ src/test/java/org/example/StateAnalyzerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StateAnalyzerTest {
-    // Test case 1.1
+
     @Test
     public void HappyTestCount() {
         StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus.csv");
@@ -13,7 +13,7 @@ class StateAnalyzerTest {
         assertEquals(count, 37);
     }
 
-    // Test 1.2
+
     @Test
     public void SadTest() {
         StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus.csv");
@@ -21,21 +21,21 @@ class StateAnalyzerTest {
         assertEquals(37, count);
     }
 
-    // Test 1.3
+
     @Test
     public void testFileType() {
         StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus.txt");
         assertEquals(37, stateCensusAnalyzer.countOfRecords());
     }
 
-    // Test 1.4
+
     @Test
     public void testDelimiter() {
         StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus2.csv");
         stateCensusAnalyzer.countOfRecords();
     }
 
-    // Test 1.5
+
     @Test
     public void testHeader() {
         StateCensusAnalyser stateCensusAnalyzer = new StateCensusAnalyser("StateCensus3.csv");
